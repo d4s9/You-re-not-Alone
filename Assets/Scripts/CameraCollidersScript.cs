058bbb8e8ff8d7b7ad6be37f66a88fa06c36b5d0
Fardox123 <yoanss25@gmail.com>
2023-02-27 11:22:39 -0500

    8

---
+++ Assets/Scripts/CameraCollidersScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollidersScript : MonoBehaviour
+{
+    [SerializeField] private GameObject _colliderVCam;
+    [SerializeField] GameObject _player = default;
+    private CameraScript _cameraScript;
+
+    void Start()
+    {
+        _cameraScript = Camera.main.gameObject.GetComponent<CameraScript>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == _player)
+        {
+            _cameraScript.OnCollisionEntering(gameObject, _colliderVCam);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == _player)
+        {
+            _cameraScript.OnCollisionLeaving();
+        }
+    }
+}
