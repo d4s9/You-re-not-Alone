d13f430843d0d43ef02ce0cc91c9a20e2b510d4d
Gabioutch <124931833+Gabioutch@users.noreply.github.com>
2023-03-09 17:12:08 -0500

    Gab
    sda

--- Assets/Scripts/Inventaire/PickupItem.cs
+++ Assets/Scripts/Inventaire/PickupItem.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PickupItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    private float pickupRange = 20000.6f;
+    public Inventaire inventaire;
+
     void Update()
     {
-        
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position,transform.forward,out hit,pickupRange))
+        {
+            if(hit.transform.CompareTag("Objet"))
+            {
+                Debug.Log("Il y a un objet");
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    inventaire.contenu.Add(hit.transform.gameObject.GetComponent<Item>().item);
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+            
+        }
     }
 }
