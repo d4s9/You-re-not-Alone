ac70b5e52eee21860a079de0074bf33f2c3e4d60
Gabioutch <124931833+Gabioutch@users.noreply.github.com>
2023-03-09 16:47:27 -0500

    Gab
    dsa

--- Assets/Scripts/Inventaire/Inventaire.cs
+++ Assets/Scripts/Inventaire/Inventaire.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class Inventaire : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public List<ItemData> contenu = new List<ItemData>();
 }
