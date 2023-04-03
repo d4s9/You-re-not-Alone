8d0bca953de69f68db087116960f51e01cbacaa0
Fardox123 <yoanss25@gmail.com>
2023-04-03 11:33:49 -0400

    1

--- Assets/Scripts/PathFinding/Unit.cs
+++ Assets/Scripts/PathFinding/Unit.cs
@@ -107,10 +107,9 @@ public class Unit : MonoBehaviour
 
     public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
     {
-        Debug.Log("Found Player");
+
         if (pathSuccessful)
         {
-            Debug.Log("Found Path");
             path = newPath;
             StopCoroutine("FollowPath");
             StartCoroutine("FollowPath");
