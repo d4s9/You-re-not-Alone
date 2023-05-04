1a49c40847df37272f3d2fced9514f927c9e0f60
Fardox123 <yoanss25@gmail.com>
2023-05-04 17:39:08 -0400

    1

--- Assets/Scripts/ReadSceneManager.cs
+++ Assets/Scripts/ReadSceneManager.cs
@@ -1,24 +1,34 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Playables;
 
 public class ReadSceneManager : MonoBehaviour
 {
+    [SerializeField] private Image FadeImage;
     private PlayableDirector cutScene;
     void Start()
     {
-        cutScene= GetComponent<PlayableDirector>();
+        cutScene = GetComponent<PlayableDirector>();
         cutScene.Play();
     }
-
     void Update()
     {
-       // if(cutScene.time > )
-        if(Time.time > cutScene.duration)
+        if(cutScene.time > 4.3f && (FadeImage != null))
         {
-            Debug.Log("Change Scene");
-            //this.GetComponent<Scene_Manager>().ChangeScene();
+            StartFade();
+        }
+        if (Time.time > cutScene.duration)
+{
+        Debug.Log("Change Scene");
+        //this.GetComponent<Scene_Manager>().ChangeScene();
         }
     }
+    private void StartFade()
+    {
+        FadeImage.gameObject.SetActive(true);
+    }
+
 }
